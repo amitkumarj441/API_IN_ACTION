03f7117dd24a7927b07e13ac8c60abc8cd95f32b
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2016-08-25 00:59:31 +0530

    Create update.py

---
+++ src/Reddit_json_cleaner/update.py
@@ -0,0 +1,24 @@
+// Author: Amit Kumar Jaiswal
+
+import json
+
+#using static paths at the moment, will become dynamic in future commits
+json_data_path = ""
+json_files =[
+            "file1",
+            "file2",
+            "etc"
+            ]
+
+for file in json_files:
+        
+    json_data = open(json_data_path + file + "_pruned.json", 'r')
+        
+    data2 = json.load(json_data)
+        
+    json_data.close()
+        
+    original_post = data2[0]
+    print(original_post)
+        
+    all_reply_threads = data2[1]["data"]["children"]
